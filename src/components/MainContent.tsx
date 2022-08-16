0469e9ece0f82ebe405f6bf58dc6d8d35daef4b2
sena-v <sethna.u@gmail.com>
2022-08-17 01:42:31 +0900

    デプロイ実施できるよう未使用箇所CO、不要configの削除

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,14 +4,14 @@
 //   <Slug post={props as Item} />
 // )}
 
-import Home from "src/components/TopPage"
-import Slug from "src/components/Post"
+import Home from "@src/components/TopPage"
+// import Slug from "@src/components/Post"
 
-import { Item } from "src/utils/read-md"
-import { AllPosts } from "src/components/TopPage"
+// import { Item } from "src/utils/read-md"
+// import { AllPosts } from "src/components/TopPage"
 
-export type Props = AllPosts | Item
+// export type Props = AllPosts | Item
 
-export const MainContent = (content: Props) => {
-  return <></>
-}
+// export const MainContent = (content: Props) => {
+//   return <></>
+// }
