cde90a48993f9075f4a34546edf1b47289e03f2a
sena-v <sethna.u@gmail.com>
2022-08-19 19:11:51 +0900

    広告領域追加テスト

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -4,7 +4,7 @@ interface googleAds extends Window {
   adsbygoogle?: any
 }
 
-function Ad(props: any) {
+const Adsense = (props: any) => {
   const { currentPath } = props
 
   useEffect(() => {
@@ -20,13 +20,41 @@ function Ad(props: any) {
         className="adsbygoogle"
         style={{ display: "block" }}
         data-ad-client="ca-pub-5515095500922112"
-        data-ad-slot="7682109354"
+        data-ad-slot="3615137069"
         data-ad-format="auto"
         data-full-width-responsive="true"
-        data-testid="adsbygoogle"
       />
     </div>
   )
 }
 
-export default Ad
+// export type Window = {
+//   adsbygoogle?: { [key: string]: unknown }[]
+// }
+
+// function Ad(props: any) {
+//   const { currentPath } = props
+
+//   useEffect(() => {
+//     // 型注釈用に中間変数に取る
+//     const currentWindow: googleAds = window
+//     currentWindow.adsbygoogle = currentWindow.adsbygoogle || []
+//     currentWindow.adsbygoogle.push({})
+//   }, [currentPath])
+
+//   return (
+//     <div>
+//       <ins
+//         className="adsbygoogle"
+//         style={{ display: "block" }}
+//         data-ad-client="ca-pub-5515095500922112"
+//         data-ad-slot="7682109354"
+//         data-ad-format="auto"
+//         data-full-width-responsive="true"
+//         data-testid="adsbygoogle"
+//       />
+//     </div>
+//   )
+// }
+
+export default Adsense
