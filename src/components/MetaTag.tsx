9a69a8909ac2f0e8c7e4395ad442a69ad2fcfc0e
sena-v <a_es_ny@yahoo.co.jp>
2023-01-23 23:11:05 +0900

    twitterCard導入テスト

---
+++ src/components/MetaTag.tsx
@@ -0,0 +1,12 @@
+export const MetaTag = () => {
+  return (
+    <>
+      <meta name="description" content="Author: sena-v" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="sena-v" />
+      <meta name="twitter:title" content="sena-v.com" />
+      <meta name="twitter:description" content="技術ブログ TyprScript/Nodejs" />
+      <meta name="twitter:image" content="https://sena-v.com/images/titleback.jpg" />
+    </>
+  );
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,21 @@
-import NextDocument, { Head, Html, Main, NextScript } from "next/document"
+import { MetaTag } from "@src/components/MetaTag";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 
 class Document extends NextDocument {
   render() {
     return (
       <Html lang="ja">
         <Head>
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5515095500922112"
-            crossOrigin="anonymous"
-          ></script>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5515095500922112" crossOrigin="anonymous"></script>
+          <MetaTag />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default Document
+export default Document;
