de8272a7a215bc1c20bd0f3e101e0faaae5ad190
sena-v <a_es_ny@yahoo.co.jp>
2023-10-27 16:03:09 +0900

    GAの設定を追加

---
+++ app/layoutClientSide.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import Script from "next/script"
+
+export const LayoutClientSide = () => {
+  return (
+    <>
+      <head>
+        <ScriptGa />
+      </head>
+    </>
+  )
+}
+
+const ScriptGa = () => {
+  return (
+    <>
+      <Script
+        defer
+        id="ga-connect"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        defer
+        id="ga-track"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", '${process.env.NEXT_PUBLIC_GA_ID}');
+        `,
+        }}
+      />
+    </>
+  )
+}
