5e73fdc2f8cafa59cee27ee8480f1de66104648b
sena-v <a_es_ny@yahoo.co.jp>
2023-11-05 12:08:58 +0900

    page.tsxにmetadata設定を移行

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
+import { Metadata } from "next"
 import Link from "next/link"
 import { getAllPosts } from "src/utils/read-md"
 
 import * as styles from "./page.css"
 
 import { PostSelectSingle } from "@/components/client/PostSelectSingle/PostSelectSingle"
-import { gitHubUrl, qiitaUrl, siteSourceCodeUrl, twitterUrl } from "@/utils/constants"
+import {
+  gitHubUrl,
+  qiitaUrl,
+  siteDescription,
+  siteSourceCodeUrl,
+  siteTitle,
+  siteUrl,
+  twitterUrl,
+} from "@/utils/constants"
+
+// 動的設定を用いてメタデータを生成する(urlが何であっても同じメタデータを返すことができる)
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: siteTitle,
+    description: siteDescription,
+    openGraph: {
+      type: "website",
+      locale: "ja_JP",
+      url: siteUrl,
+      siteName: siteTitle,
+      description: siteDescription,
+      images: `${siteUrl}/background.jpg`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: siteTitle,
+      description: siteDescription,
+    },
+  }
+}
 
 export default function Home() {
   const allPosts = getAllPosts(["slug", "title", "coverImage", "date", "tags", "content"])
