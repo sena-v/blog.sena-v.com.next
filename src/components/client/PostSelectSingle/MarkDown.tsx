4579ab9b399e00de0137467654183d56410b5a9b
sena-v <a_es_ny@yahoo.co.jp>
2023-12-26 23:32:32 +0900

    mdの要素タグ化を追加

--- src/components/client/PostSelectSingle/MarkDown.tsx
+++ src/components/client/PostSelectSingle/MarkDown.tsx
@@ -27,6 +27,30 @@ function CodeBlock({ inline, className, children }: any) {
   }
   const match = /language-(\w+)/.exec(className || "")
   const lang = match && match[1] ? match[1] : ""
+
+  // htmlTagが指定された場合、divの子要素ににそのままhtmlを埋め込む
+  if (lang === "htmlTag") {
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: children,
+        }}
+      />
+    )
+  }
+
+  // 言語にcodeSandboxが設定され、本文内にurlがある場合codeSandboxのパーツを表示する
+  if (lang === "codeSandbox") {
+    return (
+      <iframe
+        src={children}
+        style={{ width: "100%", height: "500px", border: 0, borderRadius: "4px", overflow: "hidden" }}
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      />
+    )
+  }
+
   return (
     <SyntaxHighlighter style={okaidia} language={lang}>
       {String(children).replace(/\n$/, "")}
