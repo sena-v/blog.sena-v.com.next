74621d23d3189367b3bb22e19e54cb31c7c76d6c
sena-v <a_es_ny@yahoo.co.jp>
2024-01-04 16:25:49 +0900

    ボタンのaria-label対応

--- src/components/client/ShareButton/ShareButton.tsx
+++ src/components/client/ShareButton/ShareButton.tsx
@@ -14,15 +14,15 @@ interface SharingButtonsProps {
 export function SharingButtons({ title, url }: SharingButtonsProps) {
   return (
     <div className={styles.shareButtonContainer}>
-      <FacebookShareButton url={url} title={title}>
+      <FacebookShareButton url={url} title={title} aria-label={"facebook"}>
         <FaFacebook size={30} />
       </FacebookShareButton>
       &nbsp;&nbsp;
-      <TwitterShareButton url={url} title={title}>
+      <TwitterShareButton url={url} title={title} aria-label={"twitter"}>
         <FaTwitter size={30} />
       </TwitterShareButton>
       &nbsp;&nbsp;
-      <HatenaShareButton url={url} title={title}>
+      <HatenaShareButton url={url} title={title} aria-label={"hatena"}>
         <SiHatenabookmark size={30} />
       </HatenaShareButton>
     </div>
