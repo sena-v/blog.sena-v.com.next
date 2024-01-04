90ec14223d448ac37734c9667efbd73841934dd1
sena-v <a_es_ny@yahoo.co.jp>
2024-01-04 16:51:36 +0900

    fontのpreloadを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ import { Ga4 } from "@/components/client/Ga4/Ga4"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
+        <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet" />
+      </head>
       <Ga4 />
       <SpeedInsights />
       <body>{children}</body>
