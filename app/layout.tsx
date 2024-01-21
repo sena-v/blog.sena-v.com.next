01f538fe8c38de8a3dd2dcb8c3636ffa1d04ba3c
sena-v <a_es_ny@yahoo.co.jp>
2024-01-21 17:31:47 +0900

    updによりuseSearchParamを使用する場合はSuspense必須のため修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import "../src/utils/font"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Suspense } from "react"
 
 import { Ga4 } from "@/components/client/Ga4/Ga4"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <Ga4 />
+      <Suspense>
+        <Ga4 />
+      </Suspense>
       <SpeedInsights />
       <body>{children}</body>
     </html>
