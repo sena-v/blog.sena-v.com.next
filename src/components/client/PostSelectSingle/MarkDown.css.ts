62cbccf31bfd286ec22221fb4ab9a59d59dd828b
sena-v <a_es_ny@yahoo.co.jp>
2024-02-12 11:50:17 +0900

    inlineタグを調整

---
+++ src/components/client/PostSelectSingle/MarkDown.css.ts
@@ -0,0 +1,16 @@
+import { style } from "@vanilla-extract/css"
+
+export const codeContainer = style({
+  display: "inline-flex",
+  alignItems: "center",
+  position: "relative",
+  top: "-2px",
+})
+export const code = style({
+  color: "#FFFFFF",
+  backgroundColor: "#1E1E1E",
+  borderRadius: "4px",
+  lineHeight: "25px",
+  padding: "0 5px",
+  margin: "0 2px",
+})
